94070f0d40e9006a2c1eb221bb6e1104ef29c891
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-06-24 16:02:25 -0300

    feat: show more information in the selected ability score

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -28,15 +28,27 @@
 				>
 					<v-card-text>
 						<div>Ability Score</div>
-						<p class="text-h4 text--primary">{{abilityScoresTotal.full_name}}</p>
+						<p class="text-h4 text--primary">
+							{{ abilityScoresTotal.full_name }}
+						</p>
 						<ul>
-							<li class="mb-5" v-for="(desc, index) in abilityScoresTotal.desc" :key="index">{{desc}}</li>
+							<li
+								class="mb-5"
+								v-for="(desc, index) in abilityScoresTotal.desc"
+								:key="index"
+							>
+								{{ desc }}
+							</li>
 						</ul>
-						<div class="text--primary">
-							Skills:
-						</div>
+						<div class="text--primary">Skills:</div>
 						<ul>
-							<li class="" v-for="(skill, index) in abilityScoresTotal.skills" :key="index">{{skill.name}}</li>
+							<li
+								class=""
+								v-for="(skill, index) in abilityScoresTotal.skills"
+								:key="index"
+							>
+								{{ skill.name }}
+							</li>
 						</ul>
 					</v-card-text>
 				</v-card>
@@ -47,8 +59,8 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import { dndClass } from '@/types/dndClass';
-import { abilityScores, abilityScoresFull } from '../types/abilityScoresType';
+import { dndBase } from '@/types/dndBase';
+import { abilityScoresFull } from '../types/abilityScoresType';
 
 export default Vue.extend({
 	name: 'MainPage',
@@ -57,7 +69,7 @@ export default Vue.extend({
 	},
 	computed: {
 		abilityScores() {
-			return this.$store.state.abilityScores.results as abilityScores[];
+			return this.$store.state.abilityScores.results as dndBase[];
 		},
 		abilityScoresTotal() {
 			return this.$store.state.abilityScoresTotal as abilityScoresFull;
@@ -69,7 +81,7 @@ export default Vue.extend({
 	watch: {
 		abilityScore() {
 			console.log('mudou');
-			console.log(this.$store.state.abilityScoresFull)
+			console.log(this.$store.state.abilityScoresFull);
 			this.$store.dispatch('fetchAbilityScoresTotal', this.abilityScore);
 		},
 	},
