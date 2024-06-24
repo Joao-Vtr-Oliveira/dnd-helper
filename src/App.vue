b01bcdaa592c186812786129729a079dc7359e8e
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-06-24 15:25:51 -0300

    feat: add the home page

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,52 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+	<v-app>
+		<v-app-bar
+			app
+			color="primary"
+			dark
+		>
+			<div class="d-flex align-center">
+				<v-icon>mdi-dice-d20</v-icon>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+				<h1>DnD Helper</h1>
+			</div>
 
-      <v-spacer></v-spacer>
+			<v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+			<div>
+				<v-btn class="mr-2">
+					<span>Recents</span>
+					<v-icon>mdi-history</v-icon>
+				</v-btn>
 
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+				<v-btn class="mr-2">
+					<span>Favorites</span>
+					<v-icon>mdi-heart</v-icon>
+				</v-btn>
+
+				<v-btn>
+					<span>Nearby</span>
+					<v-icon>mdi-map-marker</v-icon>
+				</v-btn>
+			</div>
+		</v-app-bar>
+		<v-main>
+			<router-view />
+		</v-main>
+	</v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
 export default Vue.extend({
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
+	name: 'App',
+	created() {
+      this.$store.dispatch('fetchClassesData');
+      this.$store.dispatch('fetchAbilityScores');
+    },
+	data: () => ({
+		value: 1,
+	}),
 });
 </script>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,80 @@
+<template>
+	<v-container>
+		<v-row class="justify-center mx-auto">
+			<v-col
+				class=""
+				cols="3"
+			>
+				<v-item-group class="">
+					<v-item
+						v-for="ability in abilityScores"
+						:key="ability.index"
+					>
+						<v-btn
+							color="primary"
+							outlined
+							x-large
+							class="m-1"
+							@click="abilityScore = ability.index"
+							>{{ ability.name }}</v-btn
+						>
+					</v-item>
+				</v-item-group>
+				<v-card
+					class="mx-auto mt-20"
+					width="500"
+					max-width="500"
+					color="light-blue darken-4"
+				>
+					<v-card-text>
+						<div>Ability Score</div>
+						<p class="text-h4 text--primary">{{abilityScoresTotal.full_name}}</p>
+						<ul>
+							<li class="mb-5" v-for="(desc, index) in abilityScoresTotal.desc" :key="index">{{desc}}</li>
+						</ul>
+						<div class="text--primary">
+							Skills:
+						</div>
+						<ul>
+							<li class="" v-for="(skill, index) in abilityScoresTotal.skills" :key="index">{{skill.name}}</li>
+						</ul>
+					</v-card-text>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { dndClass } from '@/types/dndClass';
+import { abilityScores, abilityScoresFull } from '../types/abilityScoresType';
+
+export default Vue.extend({
+	name: 'HelloWorld',
+	created() {
+		this.$store.dispatch('fetchAbilityScoresTotal', this.abilityScore);
+	},
+	computed: {
+		classes() {
+			return this.$store.state.dndClasses.results as dndClass[];
+		},
+		abilityScores() {
+			return this.$store.state.abilityScores.results as abilityScores[];
+		},
+		abilityScoresTotal() {
+			return this.$store.state.abilityScoresTotal as abilityScoresFull;
+		},
+	},
+	data: () => ({
+		abilityScore: 'cha',
+	}),
+	watch: {
+		abilityScore() {
+			console.log('mudou');
+			console.log(this.$store.state.abilityScoresFull)
+			this.$store.dispatch('fetchAbilityScoresTotal', this.abilityScore);
+		},
+	},
+});
+</script>
