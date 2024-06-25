096aeec942235854b4b9fc1780cb0b1bb4169089
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-06-25 11:00:58 -0300

    feat: add the monster type

---
+++ src/types/monsterType.ts
@@ -0,0 +1,39 @@
+import { dndBase } from './dndBase';
+
+type specialAbilities = {
+  name: string;
+  desc: string;
+}
+
+type damage = {
+  "damage_type": dndBase;
+  "damage_dice": string;
+}
+
+type actions = {
+  name: string;
+  desc: string;
+  "attack_bonus": number;
+  damage: damage[]
+}
+
+export interface monsterType extends dndBase {
+	size: string;
+	type: string;
+	subtype: string;
+	aligment: string;
+	armor_class: [{ value: number }];
+	hit_points: number;
+  speed: {
+    walk: string
+  };
+	strength: number;
+	dexterity: number;
+	constituition: number;
+	intelligence: number;
+	wisdom: number;
+	charisma: number;
+  xp: number;
+  "special_abilities": specialAbilities[];
+  actions: actions[]
+}
