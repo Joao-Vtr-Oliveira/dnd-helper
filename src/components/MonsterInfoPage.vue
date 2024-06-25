541d968b98834916f41749e6578c54de53f7947a
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-06-25 12:56:30 -0300

    chores: make the monster page responsive

--- src/components/MonsterInfoPage.vue
+++ src/components/MonsterInfoPage.vue
@@ -1,118 +1,124 @@
 <template>
 	<v-container>
-		<h1>{{ monster.name }}</h1>
 		<v-row class="justify-center">
-			<v-card
-				class="mx-auto my-12"
-				max-width="800"
-				color="light-blue darken-4"
+			<v-col
+				cols="12"
+				md="10"
+				sm="12"
+				xs="12"
+				class="d-flex flex-column align-items-center"
 			>
-				<v-card-title>{{ monster.name }}</v-card-title>
-
-				<v-card-text>
-					<v-row
-						align="center"
-						class="mx-0"
+				<v-card
+					class="mx-auto my-12 pa-4"
+					max-width="800"
+					color="light-blue darken-4"
+					raised
+				>
+					<v-card-title
+						class="headline white--text text-center justify-center"
+						>{{ monster.name }}</v-card-title
 					>
-						<div class="ms-2">
+					<v-card-text class="white--text text-center">
+						<v-row
+							align="center"
+							justify="center"
+							class="mx-0"
+						>
 							<v-chip
-								class="mr-2"
+								v-for="chip in monsterDetails"
+								:key="chip.label"
+								class="ma-2"
 								color="green"
-								v-if="monster.size"
-								>{{ monster.size }}</v-chip
+								label
 							>
-							<v-chip
-								class="mr-2"
-								color="green"
-								v-if="monster.type"
-								>{{ monster.type }}</v-chip
-							>
-							<v-chip
-								class="mr-2"
-								color="green"
-								v-if="monster.subtype"
-								>{{ monster.subtype }}</v-chip
-							>
-							<v-chip
-								class="mr-2"
-								color="green"
-								v-if="monster.aligment"
-								>{{ monster.aligment }}</v-chip
-							>
-							<v-chip
-								class="mr-2"
-								color="green"
-								v-if="monster.xp"
-								>{{ monster.xp }} XP</v-chip
-							>
+								{{ chip.label }}: {{ chip.value }}
+							</v-chip>
+						</v-row>
+
+						<div
+							class="mt-4 text-subtitle-1 white--text"
+							v-if="
+								monster && monster.armor_class && monster.armor_class.length > 0
+							"
+						>
+							HP: {{ monster.hit_points }} | CA:
+							{{ monster.armor_class[0].value }}
 						</div>
+					</v-card-text>
+
+					<v-divider class="mx-4 my-4"></v-divider>
+
+					<v-card-title class="headline white--text text-center justify-center"
+						>Ability Scores</v-card-title
+					>
+					<v-row class="justify-center">
+						<v-chip
+							v-for="(stat, index) in monsterStats"
+							:key="index"
+							class="ma-2"
+							color="primary"
+							label
+						>
+							<v-icon left>{{ stat.icon }}</v-icon>
+							{{ stat.label }}: {{ stat.value }}
+						</v-chip>
 					</v-row>
 
-					<div class="mt-4 text-subtitle-1">
-						HP: {{ monster.hit_points }} | CA:
-						{{ monster.armor_class[0].value }}
-					</div>
-				</v-card-text>
+					<v-divider class="mx-4 my-4"></v-divider>
 
-				<v-divider class="mx-4"></v-divider>
-
-				<v-card-title>Ability Scores</v-card-title>
-
-				<v-col
-					cols="12"
-					class="d-flex flex-wrap align-items-center"
-				>
-					<v-chip
-						v-for="(stat, index) in monsterStats"
-						:key="index"
-						class="ma-2"
-						color="primary"
-						label
+					<v-card-title class="headline white--text text-center justify-center"
+						>Special Abilities</v-card-title
 					>
-						<v-icon left>{{ stat.icon }}</v-icon>
-						{{ stat.label }}: {{ stat.value }}
-					</v-chip>
-				</v-col>
+					<v-row class="justify-center">
+						<v-col
+							v-for="(ability, index) in monster.special_abilities"
+							:key="index"
+							cols="12"
+							md="6"
+							sm="12"
+							class="ma-1"
+						>
+							<v-card
+								flat
+								color="transparent"
+							>
+								<h3 class="subtitle-1 white--text">{{ ability.name }}</h3>
+								<p class="body-1 white--text">{{ ability.desc }}</p>
+							</v-card>
+						</v-col>
+					</v-row>
 
-				<v-divider class="mx-4"></v-divider>
+					<v-divider class="mx-4 my-2"></v-divider>
 
-				<v-card-title>Special Abilities</v-card-title>
-
-				<v-col
-					cols="12"
-					class="d-flex flex-wrap align-items-center"
-				>
-					<div
-						v-for="(ability, index) in monster.special_abilities"
-						:key="index"
-						class="ma-2"
-						color="primary"
-						label
+					<v-card-title class="headline white--text text-center justify-center"
+						>Actions</v-card-title
 					>
-						<h1>{{ ability.name }}</h1>
-						<p>{{ ability.desc }}</p>
-					</div>
-				</v-col>
-				<v-divider class="mx-4"></v-divider>
-
-				<v-card-title>Actions</v-card-title>
-
-				<v-col
-					cols="12"
-					class="d-flex flex-wrap align-items-center"
-				>
-					<div
-						v-for="(action, index) in monster.actions"
-						:key="index"
-						class="ma-2"
-					>
-						<h1>{{ action.name }}</h1>
-						<p v-if="action.attack_bonus">Attack Bonus: {{action.attack_bonus}}</p>
-						<p>{{ action.desc }}</p>
-
-					</div>
-				</v-col>
-			</v-card>
+					<v-row class="justify-center">
+						<v-col
+							v-for="(action, index) in monster.actions"
+							:key="index"
+							cols="12"
+							md="6"
+							sm="12"
+							class="ma-1"
+						>
+							<v-card
+								flat
+								color="transparent"
+							>
+								<h3 class="subtitle-1 white--text">{{ action.name }}</h3>
+								<p
+									v-if="action.attack_bonus"
+									class="body-1 white--text"
+								>
+									Attack Bonus: {{ action.attack_bonus }}
+								</p>
+								<p class="body-1 white--text">{{ action.desc }}</p>
+							</v-card>
+						</v-col>
+					</v-row>
+				</v-card>
+			</v-col>
 		</v-row>
 	</v-container>
 </template>
@@ -165,10 +171,22 @@ export default Vue.extend({
 				},
 			];
 		},
+		monsterDetails(): { label: string; value: number | string }[] {
+			if (!this.monster) return [];
+			const details: { label: string; value: number | string }[] = [];
+			if (this.monster.size)
+				details.push({ label: 'Size', value: this.monster.size });
+			if (this.monster.type)
+				details.push({ label: 'Type', value: this.monster.type });
+			if (this.monster.subtype)
+				details.push({ label: 'Subtype', value: this.monster.subtype });
+			if (this.monster.alignment)
+				details.push({ label: 'Alignment', value: this.monster.alignment });
+			if (this.monster.xp)
+				details.push({ label: 'XP', value: this.monster.xp });
+			return details;
+		},
 	},
-	data: () => ({
-		selection: 1,
-	}),
 	created() {
 		this.$store.dispatch('fetchMonster', this.monsterId);
 	},
